e3cc559d1a754afa17483b44503a872d1767ed3f
soccerin30 <footballer.rin@gmail.com>
2022-03-18 20:25:39 +0900

    fix

--- resources/views/stamp.blade.php
+++ resources/views/stamp.blade.php
@@ -6,15 +6,15 @@
 
 @section('content')
 <div class="stamp">
-  <div class="stamp-ttl">
-    <p>さんお疲れ様です!</p>
-  </div>
-  <div class="stamp-button">
-    <button class="str-attendance" type="button">勤務開始</button>
-    <button class="fin-attendance" type="button">勤務終了</button>
-    <br>
-    <button class="str-rest" type="button">休憩開始</button>
-    <button class="fin-rest" type="button">休憩終了</button>
-  </div>
+    <div class="stamp-ttl">
+        <p>さんお疲れ様です!</p>
+    </div>
+    <div class="stamp-button">
+        <button class="str-attendance" type="button">勤務開始</button>
+        <button class="fin-attendance" type="button">勤務終了</button>
+        <br>
+        <button class="str-rest" type="button">休憩開始</button>
+        <button class="fin-rest" type="button">休憩終了</button>
+    </div>
 </div>
 @endsection
